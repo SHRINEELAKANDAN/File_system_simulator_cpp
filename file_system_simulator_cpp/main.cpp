53ed1b17b3e21bd094709439a6f5f5104c08abee
SHRINEELAKANDAN <shrineelakandan307@gmail.com>
2026-03-27 20:51:19 +0530

    Added Node class to represent file system structure using tree structure

--- file_system_simulator_cpp/main.cpp
+++ file_system_simulator_cpp/main.cpp
@@ -4,3 +4,16 @@
 
 using namespace std;
 
+// Node structure to represent files and directories
+class Node {
+public:
+    string name;
+    bool isFile;
+    map<string, Node*> children;
+
+    Node(string name, bool isFile) : name(name), isFile(isFile) {}
+};
+
+// Root directory
+Node *root= new Node("root", false);
+
